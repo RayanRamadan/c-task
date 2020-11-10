d19125d8b8b05314bc8c84b5fa53c730f6d24ac2
RayanRamadan <74253774+RayanRamadan@users.noreply.github.com>
2020-11-10 18:18:31 +0200

    frest
    aa

---
+++ c++ task/frest.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+    string student_name ,student_id;
+    char depratment;
+    int result;
+   cout<<"student name:";
+   cin>>student_name;
+    cout<<"student id:";
+    cin>>student_id;
+    cout<<"depratment";
+     cin>>depratment;
+      cout<<"result";
+       cin>>result;
+       string division;
+       char grade;
+     if(depratment ==̀ 'A')
+     division ="control";
+     else if (depratment == 'T') 
+      division ="Telecommunicaion";
+      else
+          division ="unknown";
+        if(department == 'A')
+
+Switch(department){
+case 'A';
+division="control";
+break;
+case 'T';
+division="telecommunication";
+break;
+default;
+division="unknown";
+}
+if(result>=90&&result<=100)
+grade='A';
+else if(result>=80result<90)
+grade='B'
+else if(result>=70result<80)
+grade='c'
+else if(result>=60result<70)
+grade='D'
+else(result>=50&&result<60)
+grade='f'
+Cout<<"name "student_name<<endl;
+Cout<<"ID:"<<student_id<<endl;
+Cout<<"grade:"<<grade<<endl;
+return 0;
+      
+     
+     
+}
